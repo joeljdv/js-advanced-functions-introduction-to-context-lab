c8972c19bcba6f7cac574173c33782f4c21f76eb
joeljdv <joeldiazjdv@gmail.com>
2021-03-31 13:55:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function createEmployeeRecord([string, stringTwo,stringThree, num]) {
+    const employee = {
+        firstName: string, familyName: stringTwo, title: stringThree, payPerHour: num,
+        timeInEvents:[] , timeOutEvents:[]
+    }
+    return employee
+    
+}
+
+function createEmployeeRecords(arrayOne) {
+    arrayOne.forEach(data =>{
+        createEmployeeRecord(data)
+   })
+   return arrayOne
+}
+    
+    
